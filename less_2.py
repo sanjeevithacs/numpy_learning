96dbbcd7b192dcc12d084dd3abd116b8039d72d4
sanjeevitha <sanj@gmail.com>
2024-05-20 16:00:55 +0530

    adding

--- less_2.py
+++ less_2.py
@@ -1,3 +1,8 @@
 import numpy as np
-b=np.array([[9.0,8.0,7.0],[6.0,5.0,4.0]])
+
+b = np.array([[9.0, 8.0, 7.0], [6.0, 5.0, 4.0]])
 print(b)
+print(b.shape)
+print(b[1, 1])
+print(b[0, :])
+print(b[:, 2]) 
